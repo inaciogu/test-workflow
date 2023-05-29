50836c1977fd2df6246ecdc56dc509d3f048d592
gustavo inacio <gus.inacio@hotmail.com>
2023-05-29 10:17:43 -0300

    feat: test variable

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const start = async () => {
   const { data } = await axios.get('https://pokeapi.co/api/v2/pokemon/1')
-  console.log(data)
+  console.log(process.env.TEST)
   return data
 };
 
