66a7fc1a3dff3aa00f6fb954697ece452a61e2df
gustavo inacio <gus.inacio@hotmail.com>
2023-05-30 10:58:51 -0300

    add log

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export const handler = async (event, context) => {
+  console.log("test", process.env.TEST)
   return {
     statusCode: 200,
     body: JSON.stringify({
